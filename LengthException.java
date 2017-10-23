a967e7c33c83416803c44b4b4387204765e9c132
hichem <hichem>
2017-10-23 01:44:39 +0100

    made changes

---
+++ LengthException.java
@@ -0,0 +1,14 @@
+/**
+ * Create  LengthException class from Exception
+ * @author lenovo
+ 
+ */
+public class LengthException extends Exception {
+
+	public LengthException() {
+		// TODO Auto-generated constructor stub
+		System.out.println("//////////Exceptioooooooooooon//////");
+		
+	}
+
+}
--- VectorHelper.java
+++ VectorHelper.java
@@ -17,7 +17,6 @@ public  class VectorHelper {
      * @return a sorted vector
      */
     public static int [] tri (int vector[]){
-
         /**
          * i index of a vector
          * j index of a vector
@@ -38,6 +37,28 @@ public  class VectorHelper {
             }
         }
         return ( vector ) ;
+    }
+    /**
+     *Addition of two vectors
+     * @param vector1 
+     * @param vector2
+     * @return a vector of integers as an addition of the two vectors vector1 and vector2
+     * @throws LengthException in case vectors lengths are different 
+     */
+    public static int [] Somme (int vector1[],int vector2[] ) throws LengthException{
+        int i  ;
+
+        if (vector1.length != vector2.length) throw new LengthException() ;
+        /**
+         * Vector3 is generated from the addition of vector1 with vector2
+         * i index to browse vectors
+         */
+        int vector3 [] = new int [vector1.length] ;
+        for (i = 0 ; i < vector1.length ; i++ ){
+            vector3[i] =vector1[i]+ vector2[i] ;
+        }
+        return(vector3);
+
     }
     /**
      *
@@ -62,6 +83,35 @@ public  class VectorHelper {
         }
 
         return(vector) ;
+    }
+
+    /**
+     * To extract the min and max of a vector in a vector of two cases.
+     * @param vector to extract min and max from
+     * @return a Vector of two cases , the lest one represents min and the right one is max
+     */
+    public static int[] MinMax (int vector[]){
+        /**
+         * min is minimum value of a vector
+         * max is maximum value of a vector
+         * i index to browse a vector
+         */
+         int min = vector[0] ;
+        int max = vector[0] ;
+        int i;
+        for (i=1;i<vector.length;i++){
+            if(vector[i]<min){
+                min=vector[i];
+
+            }
+            else if (vector[i]>max){
+                max=vector[i];
+            }
+        }
+        
+     int [] vecteur={min,max};
+        return (vecteur);
+
     }
     /**
 
