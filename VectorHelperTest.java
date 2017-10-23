7a6c27fec47e8574094bdf7c91843d635f977045
OuisFatimaZahra <ff_ouis@esi.dz>
2017-10-23 01:55:20 +0100

    made changes in VectorHelperTest.java

--- VectorHelperTest.java
+++ VectorHelperTest.java
@@ -14,6 +14,31 @@ public class VectorHelperTest {
 
     }
 
+    @Test
+    public void somme() throws LengthException {
+        int vector2[]={1,2,3,4} ;
+        int vector3[] ={1,1,1,1} ;
+        int vectorExpecSom []= {2,3,4,5};
+        int  TestSom [] = VectorHelper.Somme(vector2,vector3);
+
+        assertArrayEquals(vectorExpecSom,TestSom);
+
+    }
+    @Test
+    public void sommeException()  {
+        int vector2[]={1,2,3,4} ;
+        int vector3[] ={1,1,1,1,2} ;
+        int vectorExpecSom []= {2,3,4,5};
+        VectorHelper VectorHelper = new VectorHelper ();
+        try{
+        int  TestSom [] = VectorHelper.Somme(vector2,vector3);
+
+        assertArrayEquals(vectorExpecSom,TestSom);}
+        catch (LengthException e) {
+            System.out.println( "Exception tailles differentes des vecteurs ");
+        }
+
+    }
 
     @Test
     public void inverserVector()  {
@@ -23,6 +48,15 @@ public class VectorHelperTest {
         assertArrayEquals(vectorExpecIve,TestInver);
     }
 
+    @Test
+    public void minMax()  {
+        int vector5[]={1,2,3,4,5} ;
+        int vectorExpecMinMax []= {1,5};
+        int  TestMinMax [] = VectorHelper.MinMax(vector5);
+        assertArrayEquals(vectorExpecMinMax,TestMinMax);
+
+
+    }
 
     @Test
     public void formule()  {
